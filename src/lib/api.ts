3f35221fcb2ac4ae4eac65383e99bf6a7b85e188
NamjuBaek <whitenj0125@gmail.com>
2025-12-10 20:50:39 +0900

    Fix: BaseUrl 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: typeof window === 'undefined'
-        ? process.env.NEXT_PUBLIC_API_URL
-        : '/api/proxy',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
-    withCredentials: true, // 쿠키를 자동으로 포함
+    withCredentials: true,
 });
 
 export default apiClient;
